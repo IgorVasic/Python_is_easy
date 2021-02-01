378bd57175eb5fbec0e1f172a40df45b1fde3637
IgorVasic <igor25vasic@gmail.com>
2021-02-01 18:38:46 +0100

    Uploading homework

---
+++ main.py
@@ -0,0 +1,45 @@
+#this is a first homework assigment for course Python is easy
+
+artist = "Alan Walker"
+songName = "Faded"
+genre = "instrumental"
+duration = 3.33
+release = 2016
+
+print(artist)
+print(songName)
+print(genre)
+print(duration)
+print(release)
+
+"""
+This is my favorite song that is not instrumental
+
+"""
+
+artist = "Metallica"
+songName = "Nothing else matters"
+genre = ["Rock", "Metal"]
+release = 1991
+duration = 6.25
+
+print(artist)
+print(songName)
+print(genre)
+print(duration)
+print(release)
+
+
+#I listen to Placebo when I'm programming is that weird
+
+artist = "Placebo"
+songName = "Too Many Friends"
+genre = "Rock"
+duration = 3.42
+release = 2013
+
+print(artist)
+print(songName)
+print(genre)
+print(duration)
+print(release)
